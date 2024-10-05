81a19a040ac70b550f378a1fa36cde8afca9aedd
Yadav <vishuya@b0be835d7169.ant.amazon.com>
2024-10-05 11:22:28 +0530

    cross stack references

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStarterStack } from '../lib/cdk-starter-stack';
+//import { CdkStarterStack } from '../lib/cdk-starter-stack';
+import {PhotosStack} from '../lib/PhotosStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack');
+//new CdkStarterStack(app, 'CdkStarterStack');
+
+// new PhotosStack(app, 'PhotosStack');
+// new PhotosHandlerStack(app, 'PhotosHandlerStack');
+const photosStack = new PhotosStack(app, 'PhotosStack');
+new PhotosHandlerStack(app, 'PhotosHandlerStack', {
+    targetBucketArn: photosStack.photosBucketArn
+})
